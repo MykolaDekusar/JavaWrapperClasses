2c1a30aa0aa54fb9659dd8eeb12984bb47a0d0fa
MykolaDekusar <ndekusar97@gmail.com>
2024-10-28 23:22:44 +0100

    Definitions of wrapper classes

--- JavaWrapperClasses/src/Main.java
+++ JavaWrapperClasses/src/Main.java
@@ -2,7 +2,22 @@
 public class Main {
 
 	public static void main(String[] args) {
+		// Classe wrapper = fornisce un modo per utilizzare i tipi di dati primitivi come tipi di dati di riferimento
+		// i tipi di dati di riferimento contengono metodi utili
+		// possono essere utilizzati con le collezioni (es. ArrayList)
 		
+		//Es:
+		//primitive  //wrapper  
+		//---------  //------- si scrivono in questo modo tipo String
+		// boolean     Boolean
+		// char        Character
+		// int         Integer
+		// double      Double
+		
+		// Autoboxing = la conversione automatica che il compilatore Java 
+		//              effettua tra i tipi primitivi e le rispettive classi wrapper di oggetti
+		// Unboxing = l'inverso di autoboxing. Conversione automatica della classe wrapper in un tipo primitivo
+
 	}
 
 }
